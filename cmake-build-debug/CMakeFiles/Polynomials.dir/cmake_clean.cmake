44a02afc29b27f8015e877f30b253928895ba0f5
TonyT <37555343+Tonyyytran@users.noreply.github.com>
2020-07-21 13:18:11 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Polynomials.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Polynomials.dir/main.cpp.o"
+  "Polynomials.exe"
+  "Polynomials.pdb"
+  "libPolynomials.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Polynomials.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
